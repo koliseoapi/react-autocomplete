47b19e1970982ff9d2882097aacb18d1921984fa
Nacho Coloma <icoloma@gmail.com>
2016-08-23 14:35:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# React Async Autocomplete
+
+This autocompleter will display options and react to keypress (up, down, enter, esc). Inspired in [React Autocomplete](https://github.com/reactjs/react-autocomplete), but designed for a simplified workflow where requests are expected to be asynchronous (synchronous is also supported) and will display a loading icon.
+
+## Use
+
+```
+import Autocomplete from "react-async-autocomplete";
+
+// render one item on the list
+const MyItemView = function({ user }) {
+
+  return (
+    <div className="user-data">
+      <div>{user.id}</div>
+      <div>{user.name}</div>
+    </div>
+  );
+
+} 
+
+class MyApp extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      selected: undefined
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+  }
+
+  // invoked when the user types something. A delay of 200ms is 
+  // already provided to avoid DDoS'ing your own servers
+  onChange(query) {
+    
+    // you would normally do here your server access
+    fetch('/users/search?q=' + query)
+    .then((result) {
+      this.refs.autocomplete.setItems(result.body);
+    })
+  }
+
+  // called when the user clicks an option or hits enter
+  onSelect(user) {
+    this.setState({
+      selectedUser: user
+    });
+    // the returned value will be inserted into the input field. 
+    // Use an empty String to reset the field
+    return user.getName();
+  }
+
+  render() {
+    return (
+      <div>
+        <Autocomplete 
+          ref="autocomplete"
+          ItemView={MyItemView} 
+          onChange={this.onChange} 
+          onSelect={this.onSelect}
+        />
+      </div>
+    )
+  }
+
+};
+```
---
+++ test/test.js
@@ -0,0 +1,91 @@
+import Autocomplete from "../lib/main";
+import ReactDOM from "react-dom";
+import React from "react";
+import PokemonClassnames from './pokemon-classnames';
+
+// transforms pkmn-bulbasaur-shiny to Bulbasaur Shiny
+class PokemonModel {
+
+  constructor(cssClassName) {
+    this.cssClassName = cssClassName;
+    this.name = cssClassName
+      .replace(/-([a-z])/g, function (g) { return ' ' + g[1].toUpperCase(); })
+      .replace('pkmn ', '');
+  }
+}
+
+// render one pokemon 
+const PokemonItemView = function({ item }) {
+
+  return (
+    <div className="pokemon-container">
+      <div className={'pkspr ' + item.cssClassName}><i/></div>
+      <div>{item.name}</div>
+    </div>
+  );
+
+} 
+
+class MyApp extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      selected: undefined
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+  }
+
+  onChange(query) {
+    
+    // you would normally do here your server access
+    // we are delaying 1 second to see the spinner, because it's beautiful
+    this.delayed && clearTimeout(this.delayed);
+    this.delayed = setTimeout(() => {
+      this.delayed = null;
+      Promise.resolve().then(() => {
+        query = query.toLowerCase();
+        const filteredPokemon = PokemonClassnames.filter(function(cssClassName) {
+          // ignore the 'pkmn-' prefix 
+          return cssClassName.indexOf(query, 5) != -1;
+        }).map(function(cssClassName) {
+          return new PokemonModel(cssClassName)
+        })
+        this.refs.autocomplete.setItems(filteredPokemon);
+      });
+    }, 1000)
+  }
+
+  onSelect(item) {
+    this.setState({
+      selected: item
+    });
+    return item.cssClassName.replace('pkmn-', '');
+  }
+
+  render() {
+    var selected = this.state.selected;
+    return (
+      <div>
+        <Autocomplete 
+          ref="autocomplete"
+          ItemView={PokemonItemView} 
+          onChange={this.onChange} 
+          onSelect={this.onSelect}
+        />
+        { !selected? undefined :
+          <div>
+            <hr/>
+            <h3>Selected Pokemon</h3>
+            <PokemonItemView item={selected} />
+          </div>
+        }
+      </div>
+    )
+  }
+
+};
+
+ReactDOM.render(<MyApp/>, document.getElementsByClassName('autocomplete-container')[0]
+);
